fcafeee6765a15fa63e5b3c4b36c888e48b9e1d3
Krabbenhoft <67756044+Krabbenhoft@users.noreply.github.com>
2025-04-06 14:56:07 -0400

    update for today's news

--- ai.py
+++ ai.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python3
 
 from google import genai
+import os.path
+import codecs
+
+def checkUTF(fileName):
+    try:
+        currentFile = codecs.open(fileName, encoding='utf-8', errors='strict')
+        for line in currentFile:
+            pass
+        return True
+    except:
+        return False
+
 
 def getTextFromFiles():
-    pass
+    outputString = ""
+    for i in (range(1,1000)):
+        fileName = f"textinput/file{i}.txt"
+        if(not os.path.isfile(fileName)):
+            print(fileName, " is bad error!")
+            break
+        elif (not checkUTF(fileName)):
+            print(fileName, " is not UTF-8")
+        else:
+            outputString += open(fileName).read()
+            print("File is good and is: " + str(i))
+    return outputString
 
-#Basic settup of files
-keyFile = open("aikey.txt")
-indexFile = open("index.html", mode="w")
-clientKey = keyFile.readlines()[0]
-client = genai.Client(api_key=clientKey)
+print(checkUTF("indesx.html"))
+print(getTextFromFiles())
+# #Basic settup of files
+# keyFile = open("aikey.txt")
+# indexFile = open("index.html", mode="w")
+# clientKey = keyFile.readlines()[0]
+# client = genai.Client(api_key=clientKey)
 
-dataLines = open("textinput/file2.txt").read() + open("textinput/file3.txt").read() + open("textinput/file4.txt").read() + open("textinput/file5.txt").read()
+# dataLines = open("textinput/file2.txt").read() + open("textinput/file3.txt").read() + open("textinput/file4.txt").read() + open("textinput/file5.txt").read()
+# getTextFromFiles()
+# print(dataLines)
 
-print(dataLines)
-
-#Get the text
-aiPrompt = "Output a summary of recent news using this context. Format using HTML tags: " +dataLines
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents=aiPrompt
-)
-print(response.text)
-indexFile.writelines(str(response.text))
+# #Get the text
+# aiPrompt = "Output a summary of recent news using this context. Format using HTML tags: " +dataLines
+# response = client.models.generate_content(
+#     model="gemini-2.0-flash", contents=aiPrompt
+# )
+# print(response.text)
+# indexFile.writelines(str(response.text))
