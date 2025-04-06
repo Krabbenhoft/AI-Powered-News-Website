3f0ba79e0a4c8b42fa59a811eae0b502ede77104
Krabbenhoft <67756044+Krabbenhoft@users.noreply.github.com>
2025-04-06 15:30:32 -0400

    update for today's news

--- ai.py
+++ ai.py
@@ -3,6 +3,7 @@
 from google import genai
 import os.path
 import codecs
+import re
 
 def checkUTF(fileName):
     try:
@@ -13,37 +14,49 @@ def checkUTF(fileName):
     except:
         return False
 
-
 def getTextFromFiles():
     outputString = ""
+    badSite = ""
     for i in (range(1,1000)):
         fileName = f"textinput/file{i}.txt"
         if(not os.path.isfile(fileName)):
             print(fileName, " is bad error!")
+            badSite = fileName
             break
         elif (not checkUTF(fileName)):
             print(fileName, " is not UTF-8")
         else:
             outputString += open(fileName).read()
             print("File is good and is: " + str(i))
-    return outputString
+    return (outputString + "Bad site is: " + badSite)
 
-print(checkUTF("indesx.html"))
-print(getTextFromFiles())
-# #Basic settup of files
-# keyFile = open("aikey.txt")
-# indexFile = open("index.html", mode="w")
-# clientKey = keyFile.readlines()[0]
-# client = genai.Client(api_key=clientKey)
+def cleanTextFromFiles(inputText):
+    returnString = ""
+    fileArray = inputText.split("\n")
+    print(fileArray)
+    for line in fileArray:
+        if (not re.search(r'([\[\]\(\)\\\/\+\*]|http|www|\d\.)', line)):
+            print("Current match: ", re.search(r'([\[\]\(\)\\\/\+\*\_\>\<\&]|http|www|\d\.)', line))
+            returnString += line
+    return returnString
 
-# dataLines = open("textinput/file2.txt").read() + open("textinput/file3.txt").read() + open("textinput/file4.txt").read() + open("textinput/file5.txt").read()
-# getTextFromFiles()
-# print(dataLines)
+#Basic settup of files
+keyFile = open("aikey.txt")
+indexFile = open("index.html", mode="w")
+clientKey = keyFile.readlines()[0]
+client = genai.Client(api_key=clientKey)
 
-# #Get the text
-# aiPrompt = "Output a summary of recent news using this context. Format using HTML tags: " +dataLines
-# response = client.models.generate_content(
-#     model="gemini-2.0-flash", contents=aiPrompt
-# )
-# print(response.text)
-# indexFile.writelines(str(response.text))
+dataLines = (cleanTextFromFiles(getTextFromFiles()))
+getTextFromFiles()
+print(dataLines)
+
+#Get the text
+aiPrompt = """Output a summary of recent news using this context. It should be a very verbose summary of the news
+that explains all of the important things happening. Don't include random 'interest' pieces, but only the important
+stuff. Include historical background if nessesary to make a very important but brief news item longer.
+Format using HTML tags, but do not offset output with ` (backtick): """ +dataLines
+response = client.models.generate_content(
+    model="gemini-2.0-flash", contents=aiPrompt
+)
+print(response.text)
+indexFile.writelines(str(response.text))
